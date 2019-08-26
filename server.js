1f80ce6ee23c1d9112795f20987dd3c4c1e0ce67
Dupe Fadina <maafadina@hotmail.com>
2019-08-25 19:03:29 -0700

    Setting up mLab with Heroku

--- server.js
+++ server.js
@@ -29,10 +29,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapedarticlesdb", {
-  useNewUrlParser: true
-});
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 //Get a route for scraping pbsnewshour
 app.get("/scrape", function(req, res) {
